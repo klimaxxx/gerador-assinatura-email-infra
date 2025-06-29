0780c897253e42fa413dfc8cd19fa76c2f14ceb1
klimaxxx <kelton.lima@ragt.com.br>
2025-06-29 13:19:21 -0300

    feat(vpc): set default values for cidr_block, public_subnet_cidr and availability_zone

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,16 +1,19 @@
 variable "cidr_block" {
   description = "CIDR block da VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
   description = "CIDR block da subnet pública"
   type        = string
+  default     = "10.0.1.0/24"
 }
 
 variable "availability_zone" {
   description = "Zona de disponibilidade para a subnet pública"
   type        = string
+  default     = "us-east-1a"
 }
 
 variable "tags" {
