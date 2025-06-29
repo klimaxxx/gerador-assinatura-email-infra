c8f75f2422437f6bfd58ec70d83ba05fa37d0975
klimaxxx <kelton.lima@ragt.com.br>
2025-06-28 22:35:26 -0300

    feat(environment): add developer environment with fixed AMI and instance type

---
+++ terraform/environments/developer/main.tf
@@ -0,0 +1,43 @@
+module "vpc" {
+  source              = "../../modules/vpc"
+  cidr_block          = "10.30.0.0/16"
+  public_subnet_cidr  = "10.30.1.0/24"
+  availability_zone   = "us-east-1a"
+  tags = {
+    Environment = "developer"
+    Project     = "gerador-assinatura-email"
+  }
+}
+
+module "security_group" {
+  source = "../../modules/security_group"
+  name   = "dev-ec2-sg"
+  vpc_id = module.vpc.vpc_id
+  tags = {
+    Environment = "developer"
+    Project     = "gerador-assinatura-email"
+  }
+}
+
+module "ec2" {
+  source              = "../../modules/ec2"
+  ami                 = var.ami
+  instance_type       = var.instance_type
+  subnet_id           = module.vpc.public_subnet_id
+  security_group_ids  = [module.security_group.security_group_id]
+  iam_instance_profile = var.iam_instance_profile
+  user_data           = var.user_data
+  tags = {
+    Environment = "developer"
+    Project     = "gerador-assinatura-email"
+  }
+}
+
+module "ecr" {
+  source = "../../modules/ecr"
+  name   = "gerador-assinatura-email-developer"
+  tags = {
+    Environment = "developer"
+    Project     = "gerador-assinatura-email"
+  }
+} 
